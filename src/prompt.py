93ba8681f15fc13c038fde042ff1c75958d8e457
Pratik Rathod <pratikr1521998@gmail.com>
2024-02-16 10:43:15 +0530

    updated prompt

--- src/prompt.py
+++ src/prompt.py
@@ -16,7 +16,7 @@ prompt_template_technology = """
 Answer the computer science and information technology related question as detailed as possible from the provided context, and answer only from given context if the answer is not in
 provided context just say, "Answer is not available in the context kindly ask questions related to technology", don't provide the wrong answer or not to try answer from yourself.\n\n
 
-if somebody greetings then appriciate them and ask them how to help them regarding finance and banking.
+if somebody greetings then appriciate them and ask them how to help them regarding Technology.
 Context:\n {context}?\n
 Question: \n{question}\n
 
@@ -27,7 +27,7 @@ prompt_template_law = """
 Answer the law related question as detailed as possible from the provided context, and answer only from given context if the answer is not in
 provided context just say, "Answer is not available in the context kindly ask questions related to law", don't provide the wrong answer or not to try answer from yourself.\n\n
 
-if somebody greetings then appriciate them and ask them how to help them regarding finance and banking.
+if somebody greetings then appriciate them and ask them how to help them regarding Law.
 Context:\n {context}?\n
 Question: \n{question}\n
 
